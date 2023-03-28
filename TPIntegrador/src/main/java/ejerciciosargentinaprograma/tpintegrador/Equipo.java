0b338ef535bda84d7ebc552c556cc17d0bd292d9
JoaquinEMolina <94871844+JoaquinEMolina@users.noreply.github.com>
2023-03-27 21:22:27 -0300

    Primer Commit
    Pronostico +ResultadoEnum + Partido + Equipo

---
+++ TPIntegrador/src/main/java/ejerciciosargentinaprograma/tpintegrador/Equipo.java
@@ -0,0 +1,16 @@
+package ejerciciosargentinaprograma.tpintegrador;
+
+
+public class Equipo {
+    String nombre;
+    String descripcion;
+
+    public Equipo() {
+    }
+
+    public Equipo(String nombre, String descripcion) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
+    
+}
